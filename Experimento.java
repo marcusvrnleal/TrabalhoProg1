852abdf12a44ecd2bdc494a0e7d77bfd396de232
felippecosta1 <51338850+felippecosta1@users.noreply.github.com>
2019-06-05 17:03:38 -0400

    Update Experimento.java

--- Experimento.java
+++ Experimento.java
@@ -2,12 +2,24 @@ public class Experimento{
 		private int dias;
 		private int mes;
 		private int ano;
+	   	private int [] resultado = Resultado [10]
 
 		public Experimento(int dias, int mes, int ano,) {
-        this.dias = dias;
-        this.mes = mes;
-        this.ano = ano;
+        		this.dias = dias;
+			this.mes = mes;
+        		this.ano = ano;
         
     }
+	
+		públic boolean addResultado(){
+		
+		}
+	
+		public float getValoresResultados(){
+		
+		}
+	
+		public 
+	
 
 }
